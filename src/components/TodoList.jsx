b69c983c796c47cfbfc23b1de87fe6eb9f952767
mcolescott <46041167+mcolescott@users.noreply.github.com>
2024-09-10 07:18:18 -0700

    Refactored todoCard functionality to be in the TodoCard.jsx file instead of in TodoList.jsx.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
-  return (
-    <div>TodoList</div>
-  )
+    let todos = [
+        'Go to the gym',
+        'Get groceries',
+        'Drop off returns'
+    ]
+    return (
+        <ul className='main'>
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
+        </ul>
+    )
 }
